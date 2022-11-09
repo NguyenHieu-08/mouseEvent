43a23d643b8b12d6dddd433842f1db0ef27bed27
DESKTOP-V43MDD4\NGUYEN HIEU <hieunghuu08@gmail.com>
2022-11-09 12:32:18 +0700

    Upload mouse event

---
+++ script.js
@@ -0,0 +1,9 @@
+const imgObj = document.getElementById('myImage');
+function init() {
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}
+window.onload = init;
